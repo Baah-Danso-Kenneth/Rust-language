832e41b498299deedee5bc50cbd02abefe142f06
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-04-25 07:32:32 +0000

    Accept output and create mutabile variables

---
+++ l2/guessing_game/target/debug/deps/guessing_game-1043052e7fd86bf9.d
@@ -0,0 +1,5 @@
+/home/gee/bread/Rust/Rust-language/rust-course/l2/guessing_game/target/debug/deps/guessing_game-1043052e7fd86bf9: src/main.rs
+
+/home/gee/bread/Rust/Rust-language/rust-course/l2/guessing_game/target/debug/deps/guessing_game-1043052e7fd86bf9.d: src/main.rs
+
+src/main.rs:
---
+++ l2/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/gee/bread/Rust/Rust-language/rust-course/l2/guessing_game/target/debug/guessing_game: /home/gee/bread/Rust/Rust-language/rust-course/l2/guessing_game/src/main.rs
