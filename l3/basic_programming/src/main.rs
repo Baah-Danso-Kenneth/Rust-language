8cd4c3a3778f5a092426ad95af38d1e4db032751
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-04-26 16:47:40 +0000

    Shadowing technique in rust

--- l3/basic_programming/src/main.rs
+++ l3/basic_programming/src/main.rs
@@ -1,8 +1,21 @@
 fn main() {
+    // using Const
     const USER_NAME:&str = "Kenny";
+
+    // Variables
     let mut x = 5;
+
+    let q=104;
+    let q = q * 2;
+    let q = q-100;
+    let space = "          ";
+    let space = space.len();
     println!("The x value is {}", x);
     x=8;
     println!("The x value is {}",x);
     println!("The user name for this product is {}", USER_NAME);
+    println!("The value for q is now {}",q);
+    println!("The number of space s are {}",space);
+
+    // Shadowing
 }
