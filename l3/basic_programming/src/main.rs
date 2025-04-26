6b92cbad7b57d3b2bf37ee07913492291b8ca895
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-04-26 16:36:11 +0000

    Using const in code

--- l3/basic_programming/src/main.rs
+++ l3/basic_programming/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
+    const USER_NAME:&str = "Kenny";
     let mut x = 5;
     println!("The x value is {}", x);
     x=8;
     println!("The x value is {}",x);
+    println!("The user name for this product is {}", USER_NAME);
 }
